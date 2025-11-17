cb6eae7f0fa92cbc1b8bf9defc1c5899c867b1a0
Wd-Endd <endercitihacker11111@gmail.com>
2025-11-17 22:02:31 +0700

    change scrollbar

---
+++ src/Common/UI.ts
@@ -0,0 +1,8 @@
+import { Vanilla } from "asajs";
+
+// Scroll
+Vanilla.common.scrollingPanel_scrollTouch({ ignored: true })
+Vanilla.common.scrollingPanel_scrollMouse({ ignored: false })
+
+Vanilla.common.scrollBox_box_touchBox({ ignored: true })
+Vanilla.common.scrollBox_box_mouseBox({ ignored: false })
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,2 @@
 import "asajs"
+import "./Common/UI"
