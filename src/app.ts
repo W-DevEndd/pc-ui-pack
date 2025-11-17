19e79ab929acd9030ff8132c19d455bf4a50fd34
Wd-Endd <endercitihacker11111@gmail.com>
2025-11-17 21:40:41 +0700

    reset

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,1 @@
-import "./components/Buttons";
-import "./components/Common";
-import "./Modify/Common";
-import "./Modify/ConteinerScreens";
-import "./Modify/HudScreen";
-import "./Modify/ChatScreen";
-import "./Modify/UIArtAssetsCommon";
+import "asajs"
