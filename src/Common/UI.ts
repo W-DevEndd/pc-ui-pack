88e9bf2d3dc9ff0a25be3fcf1803eb72b44d0f03
Wd-Endd <endercitihacker11111@gmail.com>
2025-11-22 06:19:32 +0700

    suka blyat

--- src/Common/UI.ts
+++ src/Common/UI.ts
@@ -1,8 +1,14 @@
 import { Vanilla } from "asajs";
 
 // Scroll
-Vanilla.common.scrollingPanel_scrollTouch({ ignored: true })
-Vanilla.common.scrollingPanel_scrollMouse({ ignored: false })
+Vanilla.common.scrollingPanel_scrollTouch({ ignored: true });
+Vanilla.common.scrollingPanel_scrollMouse({ ignored: false });
 
-Vanilla.common.scrollBox_box_touchBox({ ignored: true })
-Vanilla.common.scrollBox_box_mouseBox({ ignored: false })
+Vanilla.common.scrollBox_box_touchBox({ ignored: true });
+Vanilla.common.scrollBox_box_mouseBox({ ignored: false });
+
+// Hover Slot
+Vanilla.common.highlightSlotPanel_highlight().override.addBindings();
+Vanilla.common.highlightSlotPanel_whiteBorder().override.addBindings();
+Vanilla.common.selectedItemDetailsFactory({ ignored: true });
+Vanilla.common.pocketUiContainerSlot_hover_highlightSquare({ ignored: true });
