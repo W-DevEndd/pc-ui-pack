64e5ef12c597a16321b7f52a1136e70ec8ea3eab
Wd-Endd <endercitihacker11111@gmail.com>
2025-11-22 12:54:12 +0700

    hotbar

--- src/Screens/Hud.ts
+++ src/Screens/Hud.ts
@@ -1,4 +1,15 @@
 import { Vanilla } from "asajs";
 
+// ---
+// Touch Padding
+// ---
+
 Vanilla.hud.rootPanel_bossHealthPanel_bossHudPadding({ ignored: true });
 Vanilla.hud.itemNameTextRoot_itemTextAligner_interactPadding({ ignored: true });
+
+// ---
+// Hotbar
+// ---
+
+Vanilla.hud.hotbarPanel_hotbarElipsesPanelLeft({ ignored: true });
+Vanilla.hud.hotbarPanel_hotbarElipsesPanelRight({ ignored: true });
