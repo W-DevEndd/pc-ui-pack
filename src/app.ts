f5e5fe94300404ecdda42c817ac08b268bada9f5
Wd-Endd <endercitihacker11111@gmail.com>
2025-11-22 12:38:25 +0700

    T^T

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,4 @@
 import "asajs";
 import "./Common/UI";
-import "./Screens/Hud";
-import "./Screens/Loom";
-import "./Screens/StoneCutter";
-import "./Screens/Trade2";
+import "./Screens/Containers"
+import "./Screens/Hud"
