bac31b2335e64d3651978534bc47ae005c4995f5
Wd-Endd <endercitihacker11111@gmail.com>
2025-11-30 20:49:36 +0700

    yes

--- .scripts/dev_version.js
+++ .scripts/dev_version.js
@@ -1,4 +1,7 @@
 
 const fs = require("node:fs");
 
-fs.writeFileSync("is_dev_version.json", process.argv[2]);
+fs.writeFileSync(
+    ".is_dev_version.json",
+    JSON.stringify(JSON.parse(process.argv[2]), null, 2),
+);
--- asakiyuki.global_variables.cjs
+++ asakiyuki.global_variables.cjs
@@ -3,12 +3,12 @@ const {} = require("asajs");
 
 let devVersion 
 try {
-    devVersion = require("./is_dev_version.json");
+    devVersion = require("./.is_dev_version.json");
 } catch {
     devVersion = true;
-    fs.rmSync("./is_dev_version.json", { recursive: true, force: true });
+    fs.rmSync("./.is_dev_version.json", { recursive: true, force: true });
     fs.writeFileSync(
-        "./is_dev_version.json",
+        "./.is_dev_version.json",
         JSON.stringify(true),
         { encoding: "utf-8" },
     )
