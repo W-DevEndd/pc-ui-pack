7038d7f650d2ce2ee1fe2eaa288f3070251bcaf8
Wd-Endd <endercitihacker11111@gmail.com>
2025-12-01 08:47:37 +0700

    add autocomplate chat

--- src/Screens/Chat.ts
+++ src/Screens/Chat.ts
@@ -1,11 +1,36 @@
-import { Anchor, Orientation, UI, Vanilla } from "asajs";
+import { Anchor, MappingTo, Orientation, UI, Vanilla } from "asajs";
+import { UICommon } from "../Common/UI";
 
 Vanilla.chat.chatScreenContent().modify.controls.insertFront(
     UI.stackPanel({
         ignored: "(not $endd_pcui_extra_buttons)",
         orientation: Orientation.Vertical,
-        size: ["10%", "100%c"],
-        offset: [0, 27],
+        size: ["20%", "100%c"],
+        offset: [0, -27],
         anchor: Anchor.BottomRight,
-    })
+    }).addChild(
+        UICommon.quickInvsibleButton(
+            MappingTo.ChatPreviousMessage, {
+                size: ["100%", "20%x"],
+            }
+        )
+    ).addChild(
+        UICommon.quickInvsibleButton(
+            MappingTo.ChatNextMessage, {
+                size: ["100%", "20%x"],
+            }
+        )
+    ).addChild(
+        UICommon.quickInvsibleButton(
+            MappingTo.ChatAutocompleteBack, {
+                size: ["100%", "20%x"],
+            }
+        )
+    ).addChild(
+        UICommon.quickInvsibleButton(
+            MappingTo.ChatAutocomplete, {
+                size: ["100%", "20%x"],
+            }
+        )
+    )
 )
